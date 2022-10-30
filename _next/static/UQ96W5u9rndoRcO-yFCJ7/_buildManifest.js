92967d616ea48da07f04554f2c4ab9c261268201
CodeWithArnab <98345273+CodeWithArnab@users.noreply.github.com>
2022-10-30 20:09:39 +0000

    Deploying to public from @ CodeWithArnab/portfolio@2b27da7b2041f14ababe39bfed9d16b2a944499c 🚀

---
+++ _next/static/UQ96W5u9rndoRcO-yFCJ7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-8469342c7efa61b3.js","static/chunks/pages/index-15ab2898bdc2eac2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
